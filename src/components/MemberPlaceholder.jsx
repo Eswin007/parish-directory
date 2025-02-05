2f7f3fd2f8571cd8f63fa01517b41a3da4733505
eswin.d <eswin.d@thinkpalm.com>
2025-02-06 00:41:29 +0530

    working

--- src/components/MemberPlaceholder.jsx
+++ src/components/MemberPlaceholder.jsx
@@ -1,19 +1,27 @@
-import { faInfoCircle, faUsers } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {motion } from 'framer-motion'
-import React from 'react'
+import { faInfoCircle, faUsers } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { FACTS } from "./Utilities";
 
 const MemberPlaceholder = () => {
-  return (
-    <motion.div className='member-placeholder'
-      initial={{opacity: 0, duration: .2}}
-      animate={{opacity:1, duration: .2}}
-      exit={{opacity:0, duration: .2}}
-    >
-        <FontAwesomeIcon icon={faInfoCircle}  className='member-placeholder__icon'/>
-        <span>Select a member to view the details</span>
-    </motion.div>
-  )
-}
+  const [facts, setFacts] = useState();
 
-export default MemberPlaceholder
+  return (
+    <motion.div
+      className="member-placeholder"
+      initial={{ opacity: 0, duration: 0.2 }}
+      animate={{ opacity: 1, duration: 0.2 }}
+      exit={{ opacity: 0, duration: 0.2 }}
+    >
+      <FontAwesomeIcon
+        icon={faInfoCircle}
+        className="member-placeholder__icon"
+      />
+      <div>{facts}</div>
+      <span>Select a member to view the details</span>
+    </motion.div>
+  );
+};
+
+export default MemberPlaceholder;
--- src/components/Utilities.jsx
+++ src/components/Utilities.jsx
@@ -69,3 +69,16 @@ export const BLOOD_GROUP = ["A+", "A-", "B-", "O+", "O-", "AB+", "AB-", "B+"];
 
 export const apiKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZmZndicnZpeGJtbGFibW96a3RwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU3MjkxMzgsImV4cCI6MjA0MTMwNTEzOH0.yU8fXAZa_x84GwdVhPVDdLhOWbAa6r2PoHxhnV5ebn0";
+
+export const FACTS = [
+  "The Mar Thoma presence in Ernakulam began in the early 1930s as part of the Church’s Evangelization efforts through the North Travancore Mission Field.",
+  "In 1932, Thengode Kizhakkedath Kunjunju (Varkey Korah) welcomed a mission evangelist into his home, leading to prayer sessions that evolved into a worship service.",
+  "The first church was built in 1942 on land donated by Kizhakkedath Varkey Korah, despite having only five families in the congregation.",
+  "Sunday School was first introduced by Mr. T.C. Joseph, and Qurbana services were initiated by Rev. E.V. Cherian (now Coorilos Suffragan Metropolitan) in the 1980s.",
+  "The foundation for a new church building was laid in 2009 by Coorilos Thirumeni, but financial constraints delayed construction for five years.",
+  "In 2014, the present church building was completed through a land exchange agreement with Palarivattom Mar Thoma Church, leading to its consecration by Makarios Thirumeni.",
+  "The church has grown from 5 to ~70 families, with Sunday School, Yuvajana Sakhyam, Choir, Edavaka Sanghom, Sevika Sanghom, and Senior Citizens’ Fellowship actively contributing.",
+  "The influx of IT professionals and the leadership of John John Achen, Varghese M Easow Achen, and Rev. Vijish Achen strengthened the congregation significantly.",
+  "The church continues to flourish spiritually and numerically, growing in faith and fellowship by the grace of God.",
+  "The dedication and unity of its members have played a vital role in its development, ensuring that it remains a strong spiritual and community center.",
+];
